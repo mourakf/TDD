0a034f442e8aa52e8f257c55bd446042d2cb1568
Karina Moura <57039290+mourakf@users.noreply.github.com>
2022-11-27 15:33:16 -0300

    Update APIRequestMock.test.js

--- __tests__/APIRequestMock.test.js
+++ __tests__/APIRequestMock.test.js
@@ -1,8 +1,18 @@
 const axios = require('axios');
 jest.mock('axios')
 
-describe('Should return all', () => {
-    test('test', () => {
+const mockUsers = [{
+  name: 'Alice',
+  age: 25
+}];
+const mockResponse = {
+  data: mockUsers
+};
+describe('Mock test API Users', () => {
+  test('Should return all users', async () => {
+    axios.get('/users.json') // retorna undefined
+    let data = await axios.get.mockResolvedValue(mockResponse)
+    expect(data).toHaveProperty('name')
 
-    })
+  })
 })
